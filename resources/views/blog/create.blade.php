71223f1856dd8e264d76e9e9118f3697a13d8129
Jan Alquero <janalquero@gmail.com>
2023-10-23 20:39:01 +0800

    create tables

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Laravel Blog</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="{{ url('/') }}/css/styles.css" rel="stylesheet">
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
+
     <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav" style="background-color: #343a40; background-image: url('/assets/img/home-bg.jpg'); background-size: cover;">
     <div class="container px-4 px-lg-5">
